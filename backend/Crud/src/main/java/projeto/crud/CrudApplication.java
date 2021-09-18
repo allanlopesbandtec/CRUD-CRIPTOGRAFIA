8950a16ee3564ee079070cb8c21d22194d41b46b
allanlopesbandtec <allan.lopes@bandtec.com.br>
2021-09-18 16:12:06 -0300

    Preparativos para deploy

--- backend/Crud/src/main/java/projeto/crud/CrudApplication.java
+++ backend/Crud/src/main/java/projeto/crud/CrudApplication.java
@@ -1,13 +1,8 @@
 package projeto.crud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
---
+++ backend/Crud/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into Usuario (nome,email,senha) values
+('Amanda Biagi', 'amanda.biagi@gmail.com', '$2a$10$wX9az56HFQCbro1sR8XZJ.i.rkhnCYQCWLPWQVVe3Vj3AlfiWCn4e');
+
